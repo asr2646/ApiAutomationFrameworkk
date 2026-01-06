0df69632c0aa7fe2b25e67c763717e918c300677
Abhishek <abhishekrajput301999@gmail.com>
2026-01-06 19:47:31 +0530

    Api Automation Framework

---
+++ src/test/java/org/example/tests/SampleProject/Rest_Assured_Helloworld.java
@@ -0,0 +1,7 @@
+package org.example.tests.SampleProject;
+
+public class Rest_Assured_Helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
